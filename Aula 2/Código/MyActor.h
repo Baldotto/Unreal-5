12b05fb04672f1d093e59cb023f5c4b2ed91bad7
Vinicius Baldotto <vbaldotto@testreach.com>
2024-07-10 11:44:27 -0300

    aula 1 e 2

---
+++ Aula 2/Código/MyActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class QUICKSTART_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMyActor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+	int32 TotalDamage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage") 
+	float DamageTimeInSeconds; 
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Transient, Category = "Damage") 
+	float DamagePerSecond;
+	UFUNCTION(BlueprintCallable, Category = "Damage") 
+	void CalculateValues(); 
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
